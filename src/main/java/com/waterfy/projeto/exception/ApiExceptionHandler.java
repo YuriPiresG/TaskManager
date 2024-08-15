62e1ebcd38ba5f27bebe7406f53bed315a971348
Yuri Pires <yuripiresbr@gmail.com>
2024-08-15 10:20:04 -0300

    feat: enabled https errors

--- src/main/java/com/waterfy/projeto/exception/ApiExceptionHandler.java
+++ src/main/java/com/waterfy/projeto/exception/ApiExceptionHandler.java
@@ -1,17 +1,22 @@
 package com.waterfy.projeto.exception;
 
 import java.time.ZonedDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class ApiExceptionHandler {
+
     @ExceptionHandler(value = ApiRequestException.class)
-    public ResponseEntity<Object> handleApiRequestException(
-            ApiRequestException e) {
+    public ResponseEntity<Object> handleApiRequestException(ApiRequestException e) {
         ApiException apiException = new ApiException(
                 e.getMessage(),
                 HttpStatus.BAD_REQUEST.value(),
@@ -21,13 +26,23 @@ public class ApiExceptionHandler {
     }
 
     @ExceptionHandler(value = TaskNotFoundException.class)
-    public ResponseEntity<Object> handleApiRequestException(
-            TaskNotFoundException e) {
+    public ResponseEntity<Object> handleTaskNotFoundException(TaskNotFoundException e) {
         ApiException apiException = new ApiException(
                 e.getMessage(),
                 HttpStatus.NOT_FOUND.value(),
                 HttpStatus.NOT_FOUND,
                 ZonedDateTime.now());
         return new ResponseEntity<>(apiException, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
